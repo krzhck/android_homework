25580957e0397afd22b5291352bc3abaded166b7
周雨豪 <isaaxou@icloud.com>
2022-04-12 16:41:15 +0800

    hw6 tablayout

---
+++ TabLayout/app/src/main/java/com/example/tablayout/BrowseFragment.java
@@ -0,0 +1,97 @@
+package com.example.tablayout;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.tabs.TabLayout;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link BrowseFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class BrowseFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public BrowseFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment BrowseFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static BrowseFragment newInstance(String param1, String param2) {
+        BrowseFragment fragment = new BrowseFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_browse, container, false);
+
+        // TabLayout tabLayout = getView().findViewById(R.id.tab_layout);
+        TabLayout tabLayout = (TabLayout) rootView.findViewById(R.id.tab_layout);
+
+        final ViewPager viewPager = (ViewPager) rootView.findViewById(R.id.browse_pager);
+        //final ViewPager viewPager = getView().findViewById(R.id.browse_pager);
+        final PagerAdapter adapter = new PagerAdapter
+                (getFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+
+        viewPager.addOnPageChangeListener(new
+                TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(
+                new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
+        return inflater.inflate(R.layout.fragment_browse, container, false);
+    }
+}
---
+++ TabLayout/app/src/main/java/com/example/tablayout/PagerAdapter.java
@@ -0,0 +1,40 @@
+
+package com.example.tablayout;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+/**
+ * Fragment to return the clicked tab.
+ */
+public class PagerAdapter extends FragmentStatePagerAdapter {
+    int mNumOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    /**
+     * Return the Fragment associated with a specified position.
+     *
+     * @param position
+     */
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0: return new BrowseAllFragment();
+            case 1: return new BrowseFollowedFragment();
+            default: return null;
+        }
+    }
+
+    /**
+     * Return the number of views available.
+     */
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
